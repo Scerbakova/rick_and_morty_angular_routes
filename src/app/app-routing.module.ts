5a13a452ed3ecf3ff9d2df0430aefbd1ed3f1ea5
Scerbakova <reginadilevka@inbox.lv>
2022-06-17 14:44:57 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharactersComponent } from './components/pages/characters/character-list/character-list.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'users',
+    pathMatch: 'full',
+  },
+  {
+    path: 'users',
+    component: CharactersComponent,
+  },
+  // {
+  //   path: 'user/:id',
+  //   component: Char,
+  // },
+  {
+    path: '**',
+    redirectTo: 'users',
+    pathMatch: 'full',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CharactersComponent } from './components/pages/characters/character-list/character-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, CharactersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
